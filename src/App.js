38bee23e18772f4d91ebaa8b30373639461fa820
Muradkhan0161 <murad.khan@northsouth.edu>
2020-10-27 22:02:07 +0600

    filter added handleTagClick handleFilterClick filterFunc

--- src/App.js
+++ src/App.js
@@ -6,12 +6,44 @@ import JobBoardComponent from './components/JobBoardComponent';
 
 function App() {
   const [jobs, setJobs] = useState([]);
+  const [filters, setFilters] = useState(['CSS']);
 
   useEffect(() => setJobs(data), []);
      // fetch('./assets/data.json').then(res => res.json())
     // .then((data) => {
     //   setJobs(data);
     // })
+  
+  const filterFunc = ({role, level, tools, languages}) => {
+    if(filters.length === 0) {
+      return true;
+    }
+
+    const tags = [role, level];
+
+    if(tools) {
+        tags.push(...tools);
+    }
+
+    if(languages) {
+        tags.push(...languages)
+    }
+    
+    return tags.some(tag => filters.includes(tag));
+  }
+
+  const handleTagClick = (tag) => {
+    // avoid reading the tag
+    if(filters.includes(tag)) return;
+    setFilters([...filters, tag])
+  }
+    
+  const filteredJobs = jobs.filter(filterFunc);
+
+  const handleFilterClick = (passedFilter) => {
+    setFilters(filters.filter((f) => f !== passedFilter));
+  }
+
 
   console.log(jobs);
 
@@ -20,10 +52,27 @@ function App() {
       <header className='bg-teal-500 mb-12'>
         <img src="/images/bg-header-desktop.svg" alt="bg-image" />
       </header>
+        {
+          filters.length > 0 && (
+          <div className={`flex bg-white shadow-md my-16 mx-6 p-6 rounded-md`}>
+          {filters.map(
+          (filter) => (
+          <span onClick= {() => handleFilterClick(filter)}
+          className='cursor-pointer text-teal-500 bg-teal-100 font-bold mr-4 mb-4 p-2 rounded sm:mb-0'>
+          {filter}
+          </span>
+          ))}
+          </div>
+        )}
+      
       {jobs.length === 0 ? (
         <p>Jobs are fetching ...</p>
       ): (
-        jobs.map((job) => <JobBoardComponent job={job} key={job.id} /> )
+        filteredJobs.map((job) => <JobBoardComponent 
+        job={job}
+        key={job.id} 
+        handleTagClick={handleTagClick}
+        /> )
       )}
     </div>
   );
