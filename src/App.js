a45020eaace3466465026f53b0b29bfb88698f05
Muradkhan0161 <murad.khan@northsouth.edu>
2020-10-27 16:15:29 +0600

    responsive tailwind mobile first design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React, { useState, useEffect} from 'react';
+import data from './assets/data.json';
+import JobBoardComponent from './components/JobBoardComponent';
+
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => setJobs(data), []);
+     // fetch('./assets/data.json').then(res => res.json())
+    // .then((data) => {
+    //   setJobs(data);
+    // })
+
+  console.log(jobs);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='bg-teal-500 mb-12'>
+        <img src="/images/bg-header-desktop.svg" alt="bg-image" />
       </header>
+      {jobs.length === 0 ? (
+        <p>Jobs are fetching ...</p>
+      ): (
+        jobs.map((job) => <JobBoardComponent job={job} key={job.id} /> )
+      )}
     </div>
   );
 }
 
 export default App;
+
+// TODOs
+// 1. Study the design & json
+// 2. Job Board Component
+// 3. Get the data from the JSON
+// 4. Pass down the data to the JBC
+// 5. Style it
+// 6. Filter component
+// 7. Filter the data
---
+++ src/components/JobBoardComponent.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+// {
+//     "id": 1,
+//     "company": "Photosnap",
+//     "logo": "./images/photosnap.svg",
+//     "new": true,
+//     "featured": true,
+//     "position": "Senior Frontend Developer",
+//     "role": "Frontend",
+//     "level": "Senior",
+//     "postedAt": "1d ago",
+//     "contract": "Full Time",
+//     "location": "USA Only",
+//     "languages": ["HTML", "CSS", "JavaScript"],
+//     "tools": []
+//   },
+
+const JobBoardComponent = ({ job: {
+    id,
+    company,
+    logo,
+    isNew,
+    featured,
+    position,
+    role,
+    level,
+    postedAt,
+    contract,
+    location,
+    languages,
+    tools},
+ }) => {
+    const tags = [role, level];
+
+    if(tools) {
+        tags.push(...tools);
+    }
+
+    if(languages) {
+        tags.push(...languages)
+    }
+
+    return (
+    <div className={`flex flex-col bg-white shadow-md my-16 mx-6 p-6 rounded-md ${
+        featured && 'border-teal-500 border-l-4 border-solid'
+    } sm:flex-row sm:my-4`}>
+        <div>
+            <img className='-mt-16 mb-4 w-20 h-20 sm:mt-0 sm:h-24 sm:w-24 sm:mb-0' src={logo} alt={company} />
+        </div>
+        <div className='flex flex-col justify-between ml-4' >
+            <h3 className=' font-bold text-teal-500'>
+                {company}
+                {isNew && <span className='m-2 bg-teal-500 text-teal-100 rounded-full py-1 px-2 uppercase sm:text-sm'>New</span>}
+                {featured && <span className='bg-gray-800 text-white rounded-full py-1 px-2 uppercase sm:text-sm'>featured</span>}
+            </h3>
+            <h2 className=' font-bold text-xl my-2'>{position}</h2>
+            <p className=' text-gray-700'>{postedAt} &bull; {contract} &bull; {location} </p>
+        </div>
+        <div className='flex flex-wrap items-center mt-4 mx-4 pt-4 border-t border-gray-500 sm:ml-auto sm:border-0 sm:pt-0 sm:mt-0'>
+            {tags ? 
+                tags.map((tag) => (
+                    <span className='text-teal-500 bg-teal-100 font-bold mr-4 mb-4 p-2 rounded sm:mb-0'>{tag}</span>
+                )) :
+                ''
+            }
+        </div>    
+    </div>
+)}
+
+export default JobBoardComponent;
